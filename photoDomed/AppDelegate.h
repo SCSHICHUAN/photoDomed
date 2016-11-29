3bfc75b5ea57d9e4444ac098a2807f0c121b8f88
SHICHUAN <15180796292@163.com>
2016-11-29 17:29:48 +0800

    Initial Commit

---
+++ photoDomed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  photoDomed
+//
+//  Created by SHICHUAN on 2016/11/29.
+//  Copyright © 2016年 SHICHUAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ photoDomed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  photoDomed
+//
+//  Created by SHICHUAN on 2016/11/29.
+//  Copyright © 2016年 SHICHUAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
